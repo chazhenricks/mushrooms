f8a46ff4ad6e683e7a69c6f834e3dd36a65f3038
Chaz Henricks <chazhenricks@gmail.com>
2017-05-31 15:08:36 -0500

    WORKING ON IT

--- app/app.js
+++ app/app.js
@@ -1,2 +1,12 @@
 "use strict";
 
+var app = angular.module("Mushrooms", ['ngRoute']);
+
+app.config(function($routeProvider){
+    $routeProvider
+    .when('/', {
+        templateUrl: "partials/mushroom-list.html",
+        controller: "MushroomCtrl"
+    })
+    .otherwise('/');
+});
